ede96cbd7a7033520e339e39e855a9a89e8c1629
julsy-chen <thejuliachen@gmail.com>
2024-05-22 12:08:45 -0400

    base for list of list tilemap

--- map.py
+++ map.py
@@ -4,4 +4,32 @@ class TileKind:
     def __init__(self, name, image, solidity):
         self.name = name
         self.image = pygame.image.load(image)
-        self.solidity = solidity 
+        self.solidity = solidity
+    
+
+
+class Map:
+    def __init__(self, map_file, tile_kind, tile_size):
+        self.tile_kinds = tile_kinds
+
+         # load file
+        file = open(map_file, "r")
+        data = file.read()
+        file.close()
+
+         # set up tiles from loaded data
+        self.tiles = []
+        for line in data.split("\n"):
+             row = []
+             for tile_number in line:
+                 row.append(int(tile_number))
+            
+        # set size
+        self.tile_size = tile_size
+    
+    def draw(self, screen):
+        for y, row in enumerate(self.tiles):
+            for x, tile in enumerate(row):
+                location = (x * self.tile_size, y * self.tile_size)
+                image = self.tile_kinds[tile].image
+                screen.blit(image, location)
